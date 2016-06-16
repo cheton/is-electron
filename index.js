f028c7b1169d5c4a484df5f7867a7d965f00da90
Cheton Wu <cheton@gmail.com>
2016-06-17 06:57:25 +0800

    Add the issue link

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
+// https://github.com/electron/electron/issues/2288
 function isElectron() {
     if (typeof window !== 'undefined' && window.process && window.process.type === 'renderer') {
         return true;
     }
-
     if (typeof process !== 'undefined' && process.versions && !!process.versions.electron) {
         return true;
     }
-
     return false;
 }
 
