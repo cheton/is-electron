4acaa6cab8b527c57c6e56381e6f9766d57c56b2
Cheton Wu <cheton@gmail.com>
2017-08-02 18:38:26 +0800

    Resolve an issue when the nodeIntegration option is set to false (closes #1)

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 // https://github.com/electron/electron/issues/2288
 function isElectron() {
-    if (typeof window !== 'undefined' && window.process && window.process.type === 'renderer') {
+    // Renderer process
+    if (typeof window !== 'undefined' && typeof window.process === 'object' && window.process.type === 'renderer') {
         return true;
     }
-    if (typeof process !== 'undefined' && process.versions && !!process.versions.electron) {
+
+    // Main process
+    if (typeof process !== 'undefined' && typeof process.versions === 'object' && !!process.versions.electron) {
         return true;
     }
+
+    // Detect the user agent when the `nodeIntegration` option is set to true
+    if (typeof navigator === 'object' && typeof navigator.userAgent === 'string' && navigator.userAgent.indexOf('Electron') >= 0) {
+        return true;
+    }
+
     return false;
 }
 
--- test/index.js
+++ test/index.js
@@ -1,7 +1,44 @@
-var test = require('tap').test;
+const test = require('tap').test;
+const isElectron = require('../');
 
 test('should return false in Node.js', function(t) {
     const isElectron = require('../');
     t.equal(isElectron(), false);
+    t.end();
+});
+
+test('Renderer process', function(t) {
+    const window = global.window;
+    global.window = {};
+    global.window.process = global.window.process || {};
+    global.window.process.type = 'renderer';
+
+    t.equal(isElectron(), true);
+    global.window = window;
+    t.equal(isElectron(), false);
+
+    t.end();
+});
+
+test('Main process', function(t) {
+    const electron = process.versions.electron;
+    process.versions.electron = 'x.y.z';
+
+    t.equal(isElectron(), true);
+    process.versions.electron = electron;
+    t.equal(isElectron(), false);
+
+    t.end();
+});
+
+test('Detect the user agent when the `nodeIntegration` option is set to true', function(t) {
+    const navigator = global.navigator;
+    global.navigator = {};
+    global.navigator.userAgent = 'Electron';
+
+    t.equal(isElectron(), true);
+    global.navigator = navigator;
+    t.equal(isElectron(), false);
+
     t.end();
 });
